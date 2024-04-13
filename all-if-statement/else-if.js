f55d7ac60cd27f6026ff2f32e2ba2b6bdcdd3419
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-13 22:21:57 +0600

    else if statement

---
+++ all-if-statement/else-if.js
@@ -0,0 +1,9 @@
+let Uhave = 180;
+
+if (Uhave >= 500) {
+  console.log("u can eat Beff biriyani");
+} else if (Uhave >= 400) {
+  console.log("u can eat shi Polao with Chkken rost");
+} else {
+  console.log("bhai u can eat only normal rice and curry");
+}
