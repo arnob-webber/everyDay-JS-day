626ecf1ed5b0adf20ef099a7af719c49794f2a7a
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-14 23:15:17 +0600

    easy fun js code

---
+++ all-if-statement/if and for loop/no-money-food.js
@@ -0,0 +1,13 @@
+let Uhave = prompt("how much u have");
+let YourGfHave = 10;
+
+let totalHave = Uhave + YourGfHave;
+//  console.log(totalhave)
+
+if (totalHave > 1000) {
+  alert("both of u cant eat pizza, no suficient money");
+} else {
+  for (money = 0; money >= 5; money++) {
+    alert("hurray u got free pizza");
+  }
+}
