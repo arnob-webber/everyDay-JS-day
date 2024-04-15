9fb21952cbd0d010a11f7b185306900273f289bc
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-16 00:24:04 +0600

    easy function example

--- js functions/fastFunctions.js
+++ js functions/fastFunctions.js
@@ -1,5 +1,11 @@
-function nmbr (a, b){
-    return a + b
+let mathNmbr = parseInt(prompt("wht's maths score"));
+let engNmbr = parseInt(
+  prompt("wht's english score")
+);
+ /* if u dont use parseInt then function returns a string, not a number*/
+
+function nmbr(mathNmbr, engNmbr) {
+  return mathNmbr + engNmbr;
 }
 
-console.log(nmbr(10,5))
+console.log(nmbr(mathNmbr, engNmbr));
