188cbef302493450382574ede71e79c5994f5885
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-16 00:36:23 +0600

    js function

--- js functions/fastFunctions.js
+++ js functions/fastFunctions.js
@@ -1,11 +1,16 @@
 let mathNmbr = parseInt(prompt("wht's maths score"));
-let engNmbr = parseInt(
-  prompt("wht's english score")
-);
- /* if u dont use parseInt then function returns a string, not a number*/
+let engNmbr = parseInt(prompt("wht's english score"));
+/* if u dont use parseInt() then function returns a string, not a number*/
+//  u can use parseFloat() also
 
 function nmbr(mathNmbr, engNmbr) {
   return mathNmbr + engNmbr;
 }
 
-console.log(nmbr(mathNmbr, engNmbr));
+let totalNmbr = nmbr(mathNmbr, engNmbr);
+
+if (totalNmbr < 66) {
+  alert("u dumshit");
+}
+
+// console.log(nmbr(mathNmbr, engNmbr));
