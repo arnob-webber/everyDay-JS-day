3e04251ded26d3afc9b6ff3f8abefa66e014dfbb
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-23 21:27:44 +0600

    math.pow

--- math in js/interestingMath0.js
+++ math in js/interestingMath0.js
@@ -1,8 +1,10 @@
 // the ** operator performs exponentiation its a traditional methods like Math.pow()
 
-let tally = 4**2
+let tally = 4 ** 2
 
 /*base ** exponent
   4  **    2 */
 
 console.log(tally) 
+
+console.log(Math.pow(4,2)) /*output will same 16*/
