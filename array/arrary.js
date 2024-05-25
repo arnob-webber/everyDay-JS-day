47e9327e92a885a976f877faa3d28936d201dec8
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-05-26 01:09:26 +0600

    chaning name

--- array/arrary.js
+++ array/arrary.js
@@ -2,11 +2,8 @@ let student = ["arnob", "webdeveloper", "self_taught"];
 
 if (student[0].length < 2) {
   console.log("he is op");
-}
-
-else {
-    for(  let i= student[2].length < 20; i < 4 ; i++)
-        {
-            console.log('vag')
-        }
+} else {
+  for (let arryLoop = student[2].length < 20; arryLoop < 4; arryLoop++) {
+    console.log("vag");
+  }
 }
