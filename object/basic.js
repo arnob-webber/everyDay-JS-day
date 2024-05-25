31740e7be843cc6c8cc40c82c26c553f7717c45a
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-05-25 23:40:58 +0600

    object js basic code

---
+++ object/basic.js
@@ -0,0 +1,13 @@
+const webDev = {
+  name: "arnob",
+  age: 22,
+};
+
+console.log(webDev.age);
+// console.log(webDev.name);
+
+if (webDev.age > 18) {
+  console.log("hes humble dev"+ webDev.name );
+} else {
+    console.log("hes just a lil kido")
+}
