24dc1507f9ba5eba4b6b7d344ee6da426c136a19
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-05-26 01:07:59 +0600

    arry basic

--- array/arrary.js
+++ array/arrary.js
@@ -1,5 +1,12 @@
 let student = ["arnob", "webdeveloper", "self_taught"];
 
-if (student[0].length < 6) {
+if (student[0].length < 2) {
   console.log("he is op");
+}
+
+else {
+    for(  let i= student[2].length < 20; i < 4 ; i++)
+        {
+            console.log('vag')
+        }
 }
