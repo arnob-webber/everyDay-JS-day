7273b6cf23df4902e29d7b15407c64ee5e86d85c
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-05-26 20:27:58 +0600

    element arry

--- array/adding Elements to an Array/code.js
+++ array/adding Elements to an Array/code.js
@@ -1,6 +1,9 @@
 let animal = ["panda", "dog", "cat"];
 
 animal.push("elephant"); /*dont forget to use qotes ----> '' <---- */
-// using .push its mean we'r adding element
+// using .push its mean we'r adding element. its add an element to the end
+
+// if we wanna add an element in beginning we can use unshit
+animal.unshift("tiger");
 
 console.log(animal);
