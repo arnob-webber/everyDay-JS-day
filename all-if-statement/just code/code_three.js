9c2649c773a4eecff45f64a66c37ab06e3e35638
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-06-05 23:55:18 +0600

    simple code challenge

---
+++ all-if-statement/just code/code_three.js
@@ -0,0 +1,26 @@
+// let minitues = 2;
+
+// function minitesToHour(minitues) {
+//   console.log(minitues * 60);
+// }
+
+// minitesToHour(minitues);
+let superMan = ["he can fly", "he can use laser", "he is steal"];
+var supermanFly 
+
+if (superMan.length > 0) {
+ supermanFly = 500;
+}
+
+let batmanFly = 20
+// let supermanFly = superMan.length > 0;
+// let batmanFly = 1;
+
+function superHero(supermanFly, batmanFly) {
+  if (supermanFly > batmanFly) {
+    console.log("super man is the op hero");
+  } else {
+    console.log("batman have no power just money");
+  }
+}
+superHero(supermanFly, batmanFly);
